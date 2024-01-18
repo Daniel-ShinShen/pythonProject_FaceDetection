db3b45612301d4afe149ae3b985ae85940a00f6e
Daniel-ShinShen <phy.danieljs864@gmail.com>
2024-01-18 18:01:29 +0800

    debugging the loading ui problem in hough_circles_with_gui.py

--- hough_circles_with_gui.py
+++ hough_circles_with_gui.py
@@ -7,7 +7,7 @@ import numpy as np
 from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
-from PyQt5.QtGui import QPalette
+from PyQt5.QtGui import QPalette, QPixmap
 
 from PyQt5.QtWidgets import QApplication, QWidget, QToolBar, QVBoxLayout, QHBoxLayout, QTextEdit, QPushButton, QLabel, \
     QLabel, QMessageBox, QMainWindow, QStyle, QFileDialog
@@ -40,28 +40,44 @@ class RecordVideo(QtCore.QObject):
         print(self.filename)
 
 
-class CircleDetectionWidget(QtWidgets.QWidget):
+
+
+
+class MainWindow(QMainWindow):
+    file = ''
     def __init__(self, parent=None):
         super().__init__(parent)
+        uic.loadUi("mainwindow.ui", self)
+        self.setFixedSize(QSize(1300, 850))
+        self.setWindowTitle("Face Recognition GUI")
+
+        p = self.palette()
+        p.setColor(QPalette.Window, Qt.white)
+        self.setPalette(p)
+
         self.image = QtGui.QImage()
+        self.paint_label.setPixmap(QPixmap.fromImage(self.image))
         self._red = (0, 0, 255)
         self._width = 2
         self._min_size = (30, 30)
 
-    def detect_circles(self, image: np.ndarray):
-        # Convert the image to grayscale
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # Apply median blur to reduce noise
-        gray_image = cv2.medianBlur(gray_image, 5)
-        edges = cv2.Canny(gray_image, 100, 350)
-        # Use Hough Circle Transform to detect circles
-        circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 10,
-                                  param1=50, param2=15, minRadius=14, maxRadius=30)
-        if circles is not None:
-            detected_circles = np.uint16(np.around(circles))
-            return detected_circles
-        else:
-            return []
+
+        # TODO: set video port
+        self.record_video = RecordVideo()
+
+        image_data_slot = self.image_data_slot
+        self.record_video.image_data.connect(image_data_slot)
+
+        #self.main_widget = MainWidget()
+        #self.setCentralWidget(self.main_widget)
+
+        self.setup_controll()
+    def setup_controll(self):
+        self.run_button.clicked.connect(self.start_recording)
+    def start_recording(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open Video")
+        if filename:
+            self.record_video.start_recording(filename)
 
     def image_data_slot(self, image_data):
         circles = self.detect_circles(image_data)
@@ -104,60 +120,26 @@ class CircleDetectionWidget(QtWidgets.QWidget):
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
         painter.drawImage(0, 0, self.image)
+    def detect_circles(self, image: np.ndarray):
+        # Convert the image to grayscale
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Apply median blur to reduce noise
+        gray_image = cv2.medianBlur(gray_image, 5)
+        edges = cv2.Canny(gray_image, 100, 350)
+        # Use Hough Circle Transform to detect circles
+        circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 10,
+                                  param1=50, param2=15, minRadius=14, maxRadius=30)
+        if circles is not None:
+            detected_circles = np.uint16(np.around(circles))
+            return detected_circles
+        else:
+            return []
 
-
-class MainWidget(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.circle_detection_widget = CircleDetectionWidget()
-
-        # TODO: set video port
-        self.record_video = RecordVideo()
-
-        image_data_slot = self.circle_detection_widget.image_data_slot
-        self.record_video.image_data.connect(image_data_slot)
-
-        layout = QtWidgets.QVBoxLayout()
-
-        layout.addWidget(self.circle_detection_widget)
-        self.run_button = QtWidgets.QPushButton('Start')
-        layout.addWidget(self.run_button)
-
-        self.run_button.clicked.connect(self.start_recording)
-        self.setLayout(layout)
-
-    def start_recording(self):
-        filename, _ = QFileDialog.getOpenFileName(self, "Open Video")
-        if filename:
-            self.record_video.start_recording(filename)
-
-
-class MainWindow(QMainWindow):
-    file = ''
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        uic.loadUi("mainwindow.ui", self)
-        self.setFixedSize(QSize(1300, 850))
-        self.setWindowTitle("Face Recognition GUI")
-        p = self.palette()
-        p.setColor(QPalette.Window, Qt.white)
-        self.setPalette(p)
-        self.main_widget = MainWidget()
-        self.setCentralWidget(self.main_widget)
-
-        #self.setup_controll()
-
-    #def setup_controll(self):
-
-
-
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
     script_dir = path.dirname(path.realpath(__file__))
-    main()
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
